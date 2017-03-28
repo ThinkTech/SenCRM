f53123fa4f34df94bc5c66048d5761866c612d34
Mamadou Lamine Ba <lamine.ba@thinktech.sn>
2017-03-28 13:28:02 +0000

    improving the project.

--- WebContent/modules/company/scripts/module.groovy
+++ WebContent/modules/company/scripts/module.groovy
@@ -1,40 +1,15 @@
 import org.metamorphosis.core.ActionSupport
+import org.metamorphosis.core.Search
 import org.metamorphosis.core.Mail
 import org.metamorphosis.core.MailConfig
 import org.metamorphosis.core.MailSender
 import groovy.text.markup.TemplateConfiguration
 import groovy.text.markup.MarkupTemplateEngine
 
-class Ticket {
-  long id
-  String department
-  String product
-  String subject
-  String message
-  String priority
-  Date dateCreation
-  Date dateClose
-  String status
-  def messages = []
-  def user
-}
 
-class Message {
-  Date dateCreation
-  String content
-  def user
-}
-
-class Search {
-  String filter
-  String value
-}
-
-class TicketAction extends ActionSupport {
+class ModuleAction extends ActionSupport {
 
     def entity
-	def ticket = new Ticket()
-	def message = new Message()
 	def search = new Search()
 	def tickets
 	
@@ -62,149 +37,14 @@ class TicketAction extends ActionSupport {
 	    SUCCESS
 	}
 	
-	def saveTicket()  {
-	    tickets = session.getAttribute("openedTickets")
-	    ticket.id = new Random().nextLong() + 1
-	    ticket.dateCreation = new Date()
-	    ticket.status = "on support"
-	    ticket.user = loggedUser;
-	    tickets << ticket
-	    def mailSender = new MailSender()
-	    def mail = new Mail(ticket.user.fullName,ticket.user.email,ticket.subject,getTemplate(ticket,ticket.message))
-	    mailSender.sendMail(mail,true)
+	def saveEntity()  {
 		SUCCESS
 	}
 	
 	def String execute() {
-	   println "opened tickets"
-	   tickets = session.getAttribute("openedTickets")
-	   if(!tickets) {
-	   		tickets = []
-	   		session.setAttribute("openedTickets",tickets) 
-	   }
 	   SUCCESS
 	}
 		
-	def showClosedTickets() {
-	   println "closed tickets"
-	   tickets = session.getAttribute("closedTickets")
-	   if(!tickets) {
-	   		tickets = []
-	   		session.setAttribute("closedTickets",tickets) 
-	   }
-	   SUCCESS
-	}
-	
-	def showTicket()  {
-	    Long id = Long.parseLong(request.getParameter("id"))
-	    def tickets = session.getAttribute("openedTickets")
-	    def found;
-	    for(int i=0;i<tickets.size();i++) {
-	        if(id.equals(tickets[i].id)) {
-	           ticket = tickets[i]
-	           found = true
-	           break
-	        }
-	    }
-	    if(!found) {
-	       tickets = session.getAttribute("closedTickets") ? session.getAttribute("closedTickets") : [] 
-	       for(int i=0;i<tickets.size();i++) {
-	        if(id.equals(tickets[i].id)) {
-	           ticket = tickets[i]
-	           found = true
-	           break
-	        }
-	       }
-	    }
-	    found ? SUCCESS : ERROR
-	}
-	
-	def addMessage()  {
-	    tickets = session.getAttribute("openedTickets")
-	    for(int i=0;i<tickets.size();i++) {
-	        if(ticket.id.equals(tickets[i].id)) {
-	           message.dateCreation = new Date()
-	           message.user = loggedUser
-	           tickets[i].messages << message
-	           if(message.user.role.equals("customer")) {
-	              tickets[i].status = "on support"
-	           }else {
-	              tickets[i].status = "on customer"
-	           }
-	           break
-	        }
-	    }
-	    def mailSender = new MailSender()
-	    def mail = new Mail(message.user.fullName,message.user.email,ticket.subject,getTemplate(ticket,message.content))
-	    mailSender.sendMail(mail,true)
-		response.sendRedirect(request.getHeader("referer"))
-	}
-	
-	def closeTicket()  {
-	    Long id = Long.parseLong(request.getParameter("id"))
-	    def tickets = session.getAttribute("openedTickets")
-	    def closedTickets = session.getAttribute("closedTickets") ? session.getAttribute("closedTickets") : [] 	
-	    def index
-	    for(int i=0;i<tickets.size();i++) {
-	        if(id.equals(tickets[i].id)) {
-	           tickets[i].dateClose = new Date()
-	           tickets[i].status = "closed"
-	           closedTickets << tickets[i]
-	           index = i
-	           break
-	        }
-	    }
-	    tickets.remove(index)
-	    session.setAttribute("openedTickets",tickets) 
-	    session.setAttribute("closedTickets",closedTickets) 
-		SUCCESS
-	}
-	
-	def searchOpenedTickets() {
-	    println "search opened tickets"
-	    def openedTickets = session.getAttribute("openedTickets")
-	    tickets = []
-	    if(search.value) {
-		    for(ticket in openedTickets) {
-		       if(search.filter.equals("id")) {
-		          if(ticket.id.equals(new Long(search.value))) {
-		             tickets << ticket
-		          }
-		       }else if(search.filter.equals("subject")) {
-		          println ticket.subject
-		          if(ticket.subject.startsWith(search.value)) {
-		             tickets << ticket
-		          }
-		       }
-		    }
-		  SUCCESS  
-	    }else {
-	     execute()
-	    }
-	}
-	
-	def searchClosedTickets() {
-	    println "search closed tickets"
-	    def closedTickets = session.getAttribute("closedTickets")
-	    tickets = []
-	    if(search.value) {
-		    for(ticket in closedTickets) {
-		       if(search.filter.equals("id")) {
-		          if(ticket.id.equals(new Long(search.value))) {
-		             tickets << ticket
-		          }
-		       }else if(search.filter.equals("subject")) {
-		          println ticket.subject
-		          if(ticket.subject.startsWith(search.value)) {
-		             tickets << ticket
-		          }
-		       }
-		    }
-		  SUCCESS  
-	    }else {
-	     showClosedTickets()
-	    }
-	}
 	
 	def getTemplate(ticket,message) {
 	    TemplateConfiguration config = new TemplateConfiguration()
@@ -239,4 +79,4 @@ class TicketAction extends ActionSupport {
 		
 }
 
-new TicketAction()
+new ModuleAction()
